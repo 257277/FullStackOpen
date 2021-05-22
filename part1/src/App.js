5a376d6d4ff4a386cfe0d32fa81d1625dba01115
Rahul sharma <76rahul257@gmail.com>
2021-05-22 17:05:31 +0530

    Adding 1.4: course information step4

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,39 +4,49 @@ function Header(props)
 {
   return <h1>{props.name}</h1>
 }
+
 function Part(props)
 {
-  return <p>{props.name} {props.text}</p>
+  return <div>
+            <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+            <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+            <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+          </div>
+  
 }
+
+
+  
 
 function Total(props)
 {
-  return <p>Number of exercises {props.text1+props.text2+props.text3}</p>
+  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 }
 
 
 const App=()=>{
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
+  const parts = [
+   { name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
  
   return (
     <div>
     <Header name={course} />
-    <Part name={part1} text={exercises1} />
-    <Part name={part2} text={exercises2} />
-    <Part name={part3} text={exercises3} />
-    <Total text1={exercises1} text2={exercises2} text3={exercises3} />
+    <Part parts={parts}/>
+    <Total parts={parts}/>
+  
+   
     </div>
   )
   }
